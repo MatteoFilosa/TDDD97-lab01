74731bd63819ca586bd10a2cbb713d094df1f9de
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2022-01-25 17:54:12 +0100

    .js part!

---
+++ client.js
@@ -0,0 +1,87 @@
+function validateLogin() {
+
+  let username = document.forms["login"]["username"].value;
+  let password = document.forms["login"]["password"].value;
+  Match match = regex.Match(username);
+  Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+  if (username == "") {
+    document.getElementById('log').innerHTML = "Username cannot be empty!";
+    return false;
+  }
+
+  if (!match.Success){
+    document.getElementById('log').innerHTML = "Bad e-mail format!";
+    return false;
+  }
+  if (password == "") {
+    document.getElementById('log').innerHTML = "Password cannot be empty!";
+    return false;
+  }
+  if (password.length()<5){
+    document.getElementById('log').innerHTML = "Password cannot be shorter than 5 characters.";
+    return false;
+  }
+
+  let dataObject = [username, password, firstName, familyName, gender, city, country];
+  serverstub.signIn(dataObject);
+}
+
+function validateSignUp() {
+  let username = document.forms["signUp"]["username"].value;
+  let password = document.forms["signUp"]["password"].value;
+  let firstName = document.forms["signUp"]["firstName"].value;
+  let familyName = document.forms["signUp"]["familyName"].value;
+  let city = document.forms["signUp"]["city"].value;
+  let country = document.forms["signUp"]["country"].value;
+  let rPassword = document.forms["signUp"]["rPassword"].value;
+  Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+  Match match = regex.Match(username);
+
+  if (username == "") {
+    document.getElementById('log').innerHTML = "Username cannot be empty!";
+    return false;
+  }
+
+  if (!match.Success){
+    document.getElementById('log').innerHTML = "Bad e-mail format!";
+    return false;
+  }
+
+  if (firstName == "") {
+    document.getElementById('log').innerHTML = "Name cannot be empty!";
+    return false;
+  }
+
+  if (familyName == "") {
+    document.getElementById('log').innerHTML = "Family name cannot be empty!";
+    return false;
+  }
+
+  if (city == "") {
+    document.getElementById('log').innerHTML = "City cannot be empty!";
+    return false;
+  }
+
+  if (country == "") {
+    document.getElementById('log').innerHTML = "Country cannot be empty!";
+    return false;
+  }
+
+
+  if (password == "") {
+    document.getElementById('log').innerHTML = "Password cannot be empty!";
+    return false;
+  }
+  if (password.length()<5){
+    document.getElementById('log').innerHTML = "Password cannot be shorter than 5 characters.";
+    return false;
+  }
+
+  if(password != rPassword){
+    document.getElementById('log').innerHTML = "Passwords should be equal!";
+    return false;
+  }
+  let dataObject = [username, password, firstName, familyName, gender, city, country];
+  serverstub.signUp(dataObject);
+}
