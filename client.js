9cb53099ca463887d220a3755f14538ff6398816
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2022-02-14 16:25:01 +0100

    again

--- client.js
+++ client.js
@@ -104,7 +104,7 @@ function validateSignUp() {
 }
 
 //TABS
-function openTab(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -121,7 +121,7 @@ function openTab(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
